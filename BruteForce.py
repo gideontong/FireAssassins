5bd89651d787e1f4bc0c781badbe73490e2ba85c
Gideon Tong <gideontong@splimtech.com>
2019-01-30 23:50:59 -0800

    Fixing the screwup

--- BruteForce.py
+++ BruteForce.py
@@ -5,7 +5,7 @@ key = 1548716400
 
 filename = "TeamRandomizer.py"
 while True:
-    print("\nNow attempting key: " + str(key))
+    print("Now attempting key: " + str(key))
     p = Popen("python " + filename + " " + str(key), shell = True)
     key += 1
     p.wait()
