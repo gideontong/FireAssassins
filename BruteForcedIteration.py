1fcb7aee8f1ec57b253b588d40780eaca5b44e5f
Gideon Tong <gideontong@splimtech.com>
2019-01-31 20:35:52 -0800

    Fixed stupid loop bug

--- BruteForcedIteration.py
+++ BruteForcedIteration.py
@@ -1,15 +1,16 @@
 from subprocess import Popen
 import sys
+import time
 
 keylist = open('pwned.txt')
 filename = "TeamRandomizer.py"
-nextKey = keylist.readline()
 i = 0
 
-while nextKey:
+for nextKey in keylist:
     i += 1
     nextKey = nextKey[0:9]
-    f = open('output' + nextKey + '.txt', 'w+')
-    print("Iteration " + str(i) + "/87")
+    '''f = open('output' + nextKey + '.txt', 'w+')'''
+    print("Iteration " + str(i) + "/87 with key " + nextKey)
     p = Popen("python " + filename + " " + nextKey, shell = True)
     nextKey = keylist.readline()
+    time.sleep(1)
