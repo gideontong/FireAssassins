d6d150557477a4297988824f18b28a4fb7fb7ecc
Gideon Tong <gideon@splimtech.com>
2019-02-03 16:55:50 -0800

    Correct version of Mersenne Twister

--- BruteForce.py
+++ BruteForce.py
@@ -2,7 +2,7 @@ from subprocess import Popen
 import sys
 
 # key = 1548716400
-key = 1548725657
+key = 1548716400
 # realTarget = 1548727920
 target = 1548727920
 
@@ -10,6 +10,6 @@ filename = "TeamRandomizer.py"
 
 while key < target:
     print("Now attempting key: " + str(key))
-    p = Popen("python " + filename + " " + str(key), shell = True)
+    p = Popen("python3 " + filename + " " + str(key), shell = True)
     key += 1
     p.wait()
