34061f7407ff110486230186bbef1fb9c07dea80
Gideon Tong <gideon@splimtech.com>
2019-02-09 15:16:50 -0800

    Some code moving

--- BruteForce.py
+++ BruteForce.py
@@ -15,10 +15,10 @@ from subprocess import Popen
 import sys
 
 # key = 1548716400
-key = 1548726921
 # realTarget = 1548727920
-target = 1548727920
 
+key = 1548726921
+target = 1548727920
 filename = "TeamRandomizer.py"
 
 while key < target:
